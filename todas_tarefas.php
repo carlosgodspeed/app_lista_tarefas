5912458770f57e24e45a3cbdc3c6d41d09dc645b
carlosgodspeed <carlosminibics@gmail.com>
2023-07-17 16:18:51 -0300

    atualização do projeto

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -24,7 +24,7 @@
 			function editar(id, txt_tarefa) {
 				//form de edição
                 let form = document.createElement('form')
-				form.action = '#'
+				form.action = 'tarefa_controller.php?acao=atualizar'
 				form.method = 'post'
 				form.className='row'
 
